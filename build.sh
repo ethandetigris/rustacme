413cbf6075a561c6fee518c169badb541cdbc586
Jia <165471121+ethandetigris@users.noreply.github.com>
2026-06-27 02:17:43 +0800

    Allow local Cargo config override

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")"
 
 echo "=== Building rustacme ==="
-docker build -t rustacme:local .
+docker build --build-arg CARGO_CONFIG="${RUSTACME_CARGO_CONFIG:-.cargo/config.toml}" -t rustacme:local .
 echo "=== Starting ==="
 RUSTACME_IMAGE=rustacme:local docker compose up -d
 echo "=== Done. Check logs: docker logs rustacme ==="
